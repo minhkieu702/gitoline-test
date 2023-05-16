390f74e378cb8edda7e4d75ace5f885022dad391
Minh <minhkieu702@gmail.com>
2023-05-16 10:30:49 +0700

    Presentatioinal and Container Components

--- webreactjs247/src/App.js
+++ webreactjs247/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
 import Player from './Players';
+import Main from './components/Main';
 import Footer from './Footer';
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
-      <Player></Player>
+      {/* <Player></Player> */}
+      <Main></Main>
       <Footer></Footer>
     </div>
   );
--- webreactjs247/src/Players.js
+++ webreactjs247/src/Players.js
@@ -1,34 +1,7 @@
-import React, {Component} from "react";
+import React from "react";
 import { Players } from "./shared/ListOfPlayers";
-export default class Player extends Component{
-    render(){
+export default function Player() {
         return(
-            // <div className="container">
-            //     <div className="column">
-            //         <div className="card">
-            //         <img src={cr}/>
-            //             <h3>Cristiano Ronaldo</h3>
-            //             <p className="title">Manchester United</p>
-            //             <p><button>Detail</button></p>
-            //         </div>
-            //     </div>
-            //     <div className="column">
-            //         <div className="card">
-            //         <img src={haaland}/>
-            //             <h3>haaland</h3>
-            //             <p className="title">Manchester City</p>
-            //             <p><button>Detail</button></p>
-            //         </div>
-            //     </div>
-            //     <div className="column">
-            //         <div className="card">
-            //         <img src={neymar}/>
-            //             <h3>neymar</h3>
-            //             <p className="title">PSG</p>
-            //             <p><button>Detail</button></p>
-            //         </div>
-            //     </div>
-            // </div>
             <div className="container">
                 {Players.map((player)=>(
                     <div className="column">
@@ -41,6 +14,5 @@ export default class Player extends Component{
                 </div>
                 ))}
             </div>
-        )       
-    }
+        )
 }
