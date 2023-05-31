82bf414caadcfffad046775358d84b146593ff8c
Minh <minhkieu702@gmail.com>
2023-05-31 10:35:38 +0700

    Components part 2

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,3 +16,17 @@ export default class Navigation extends Component{
         )
     }
 }
+// export default function Navigation() {
+//     return(
+//             <div className="navigation">
+//                 <nav>
+//                     <ul>
+//                         <li><a className="active" href="#home">Home</a></li>
+//                         <li><a href="#news">News</a></li>
+//                         <li><a href="#about">About</a></li>
+//                         <li><a href="#contact">Contact</a></li>
+//                     </ul>
+//                 </nav>
+//             </div>
+//     )
+// }
