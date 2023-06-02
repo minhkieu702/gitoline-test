857e1f71d368809adac3751cb5160a67e47084c9
Minh <minhkieu702@gmail.com>
2023-06-02 17:18:23 +0700

    switch

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import PlayersPresentation from './components/PlayersPresentation';
+import Footer from './components/Footer';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+function App() {
+  return (
+    <div className="App">
+      <Navigation></Navigation>
+      <PlayersPresentation></PlayersPresentation>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import { players } from "./ListOfPlayers";
+import PlayersPresentation from "./PlayersPresentation";
+export class Main extends Component{
+    constructor(){
+        super();
+        this.state = {players: players};
+    }
+    render(){
+        return <PlayersPresentation players={this.state.players}></PlayersPresentation>
+    }
+}
+export default Main
